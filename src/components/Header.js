8ed1edf289b6a8f98d4b19bcc18117e253627666
talhaunas1 <mtalha4600@gmail.com>
2022-07-08 11:53:32 +0500

    push

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,52 @@
-// import React from 'react'
+import React from 'react'
 import styled from 'styled-components'
+import { auth, provider } from '../firebase'
 
 const Header = () => {
-  return (
-    <Nav>
-        <Logo>
-            <img src="/images/logo.svg" alt='logo'/>
-        </Logo>
-        <NavMenu>
-            menu
-        </NavMenu>
-    </Nav>
-  )
+
+    // const handleAuth = () => {
+    //     auth.signInWithPopup(provider).then((result) => {
+    //         console.log(result)
+    //     })
+    //         .catch((error) => {
+    //             alert(error.message);
+    //         })
+    // }
+    return (
+        <Nav>
+            <Logo>
+                <img src="/images/logo.svg" alt='logo' />
+            </Logo>
+            <NavMenu>
+                <a href='/home'>
+                    <img src="/images/home-icon.svg" alt="HOME" />
+                    <span>HOME</span>
+                </a>
+                <a href='/home'>
+                    <img src="/images/search-icon.svg" alt="SEARCH" />
+                    <span>SEARCH</span>
+                </a>
+                <a href='/home'>
+                    <img src="/images/watchlist-icon.svg" alt="WATCHLIST" />
+                    <span>WATCHLIST</span>
+                </a>
+                <a href='/home'>
+                    <img src="/images/original-icon.svg" alt="ORIGINALS" />
+                    <span>ORIGINALS</span>
+                </a>
+                <a href='/home'>
+                    <img src="/images/movie-icon.svg" alt="MOVIES" />
+                    <span>MOVIES</span>
+                </a>
+                <a href='/home'>
+                    <img src="/images/series-icon.svg" alt="SERIES" />
+                    <span>SERIES</span>
+                </a>
+
+            </NavMenu>
+            <Login >Login</Login>
+        </Nav>
+    )
 }
 
 const Nav = styled.nav`
@@ -55,10 +90,76 @@ padding:0px ;
 position:relative ;
 margin-right:auto ;
 margin-left: 25px;
+/* background-color:blue ; */
+ a{
+    display:flex ;
+    align-items: center ;
+    padding:0 12px ;
+   
+    img{
+        height:20px;
+        min-width:20px ;
+        width:20px ;
+        z-index:auto;
+    }
 
-@media (max-width:768px){
+
+    span {
+        color:#F9F9F9 ;
+        font-size:13px ;
+        letter-spacing:1.42px ;
+        line-height:1.08 ;
+        padding:2px 0px ;
+        white-space:nowrap ;
+        position:relative ;
+
+        &:before {
+        
+        /* display:block ; */
+        background-color:rgb(249,249,249) ;
+        border-radius:0px 0px 4px 4px ;
+        bottom:-6px ;
+        content: "" ;
+        height: 2px ;
+        left:0px ;
+        opacity:0 ;
+        position:absolute ;
+        right:0 ;
+        transform-origin:left center ;
+        transform:scaleX(0) ;
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94)0s ;
+        visibility: hidden;
+        width:auto ;
+        }
+    }
+
+    &:hover {
+        span:before{
+            transform: scaleX(1) ;
+            visibility: visible;
+            opacity:1 !important ;
+        }
+    }
+
+ }
+/* @media (max-width:768px){
     display:none ;
-}
+} */
 `
 
+const Login = styled.a`
+background-color: rgba(0,0,0,0.6) ;
+border:1px solid #f9f9f9;
+border-radius:4px ;
+padding:8px 16px ;
+text-transform:uppercase;
+letter-spacing:1.5px ;
+transition:all 0.2s ease 0s ;
+
+&:hover{
+    background-color: #f9f9f9;
+    color:#000 ;
+    border-color:transparent ;
+}
+`
 export default Header
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// import firebase from "firebase"
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyD_2hvtO_xyMftIjwUqylmP5J4bPjD_yPo",
+//     authDomain: "disneypplus-clone.firebaseapp.com",
+//     projectId: "disneypplus-clone",
+//     storageBucket: "disneypplus-clone.appspot.com",
+//     messagingSenderId: "783745651253",
+//     appId: "1:783745651253:web:cd3ad6a113e829ac5a577c",
+//     measurementId: "G-BDJH0L07SZ"
+//   };
+
+//   const firebaseApp = firebase.initializeApp(firebaseConfig);
+//   const db = firebaseApp.firestore();
+//   const auth = firebase.auth;
+//   const provider = new firebase.auth.GoogleAuthProvider();
+//   const storage = firebase.storage();
+
+//   export {auth, provider, storage };
+//   export default db;
