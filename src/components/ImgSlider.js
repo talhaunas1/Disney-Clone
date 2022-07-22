8e3bf55ae1ebac7a8b1b47e2d2f067be471b46bd
talhaunas1 <mtalha4600@gmail.com>
2022-07-22 16:34:03 +0500

    adding react-slick for slider

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import Slider from 'react-slick'
+import { toBeRequired } from '@testing-library/jest-dom/dist/matchers'
 
 function ImgSlider() {
+
+    const settings = {
+        dots :true,
+        Infinity:true,
+        speed:500,
+        slidesToShow:1,
+        slidesToScroll:1,
+        autoplay: true,
+    }
+
   return (
-    <div>ImgSlider</div>
+    <div>
+        <Slider { ...settings}>
+            <div>
+                <h3>1</h3>
+            </div>
+            <div>
+                <h3>2</h3>
+            </div>
+        </Slider>
+    </div>
   )
 }
 
