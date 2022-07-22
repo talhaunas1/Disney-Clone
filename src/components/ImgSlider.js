c1068127140afa1e940c312d6f2e99868e931e62
talhaunas1 <mtalha4600@gmail.com>
2022-07-22 17:19:18 +0500

    adding react-slick for slider

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -2,7 +2,8 @@ import React from 'react'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import Slider from 'react-slick'
-import { toBeRequired } from '@testing-library/jest-dom/dist/matchers'
+import styled from 'styled-components'
+// import { toBeRequired } from '@testing-library/jest-dom/dist/matchers'
 
 function ImgSlider() {
 
@@ -16,17 +17,64 @@ function ImgSlider() {
     }
 
   return (
-    <div>
-        <Slider { ...settings}>
+   
+        <Carousel { ...settings}>
             <div>
                 <h3>1</h3>
             </div>
             <div>
                 <h3>2</h3>
             </div>
-        </Slider>
-    </div>
+            <div> 
+                <h3>3</h3>
+            </div>
+        </Carousel>
+   
   )
 }
+
+const Carousel = styled(Slider)`
+margin-top:20px ;
+
+& > button{  
+    opacity:0 ;
+    height:100% ;
+    width:5vw ;
+    z-index:1;
+
+
+    &:hover {
+      opacity: 1;
+      transition: opacity 0.2s ease 0s;
+      /* the sides button  of slider shows up when hover */
+    }
+}
+
+ul li button {
+    &:before {
+        font-size:10px ;
+        color:rgb(150,159,171);
+        /* the dots button sizze up */
+    }
+}
+
+li.slick-active button:before{
+    color:white ;
+    /* the dots were becomes white  */
+}
+
+.slick-list{
+    overflow:initial ; 
+    /* we can see the upcoming one 1     2 */
+}
+
+.slick-prev{
+    left: -75px;
+}
+
+.slick-next{
+    right:-75px ;
+}
+`
 
 export default ImgSlider
