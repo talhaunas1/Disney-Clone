2c965bcd9abddfbe969f61883a5a9b5d6ad55f61
talhaunas1 <mtalha4600@gmail.com>
2022-07-23 13:00:27 +0500

    working of viewer page

--- src/components/Viewrs.js
+++ src/components/Viewrs.js
@@ -2,27 +2,42 @@ import React from 'react'
 import styled from 'styled-components'
 
 function Viewrs() {
-  return (
+    return (
 
-    <Container>
-        <Wrap>
-            <img src='/images/viewers-disney.png' />
-        </Wrap>
-        <Wrap>
-        <img src='/images/viewers-pixar.png' />
-        </Wrap>
-        <Wrap>
-        <img src='/images/viewers-marvel.png' />
-        </Wrap>
-        <Wrap>
-        <img src='/images/viewers-starwars.png' />
-        </Wrap>
-        <Wrap>
-        <img src='/images/viewers-national.png' />
-        </Wrap>
-    </Container>
+        <Container>
+            <Wrap>
+                <img src='/images/viewers-disney.png' />
+                <video autoPlay={true} loop={true} playsInline={true}>
+                    <source src='/videos/1564674844-disney.mp4' type='video/mp4' />
+                </video>
+            </Wrap>
+            <Wrap>
+                <img src='/images/viewers-pixar.png' />
+                <video autoPlay={true} loop={true} playsInline={true}>
+                    <source src='/videos/1564676714-pixar.mp4' type='video/mp4' />
+                </video>
+            </Wrap>
+            <Wrap>
+                <img src='/images/viewers-marvel.png' />
+                <video autoPlay={true} loop={true} playsInline={true}>
+                    <source src='/videos/1564676115-marvel.mp4' type='video/mp4' />
+                </video>
+            </Wrap>
+            <Wrap>
+                <img src='/images/viewers-starwars.png' />
+                <video autoPlay={true} loop={true} playsInline={true}>
+                    <source src='/videos/1608229455-star-wars.mp4' type='video/mp4' />
+                </video>
+            </Wrap>
+            <Wrap>
+                <img src='/images/viewers-national.png' />
+                <video autoPlay={true} loop={true} playsInline={true}>
+                    <source src='/videos/1564676296-national-geographic.mp4' type='video/mp4' />
+                </video>
+            </Wrap>
+        </Container>
 
-  )
+    )
 }
 
 const Container = styled.div`
@@ -37,7 +52,7 @@ grid-template-columns: repeat(5, minmax(0, 1fr));
     grid-template-columns: repeat(1, minmax(0, 1fr));
 }
 `
- const Wrap = styled.div`
+const Wrap = styled.div`
  padding-top: 56.25% ;
 border-radius:10px ;
 box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
@@ -62,5 +77,26 @@ img{
     z-index: 1;
     top: 0;
 }
+
+video {
+    width:100% ;
+    height:100% ;
+    position:absolute ;
+    top:0px ;
+    opacity:0 ;
+    z-index:0;
+}
+
+ &:hover{
+    box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
+      rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+
+    transform:scale(1.05) ;
+    border-color: rgba(249,249,249,0.8);
+    
+    video{
+        opacity:1 ;
+    }
+ }
 `
 export default Viewrs
