c6710e98cccdeb854419e8fd645064adbd9553d0
talhaunas1 <mtalha4600@gmail.com>
2022-07-24 23:01:53 +0500

    styling details component

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -10,6 +10,27 @@ function Detail() {
         <ImgTitle>
           <img src="" alt="" />
         </ImgTitle>
+        <ContentMeta>
+          <Controls>
+            <Player>
+              <img src="/images/play-icon-black.png" />
+              <span>Play</span>
+            </Player>
+            <Trailer>
+            <img src="/images/play-icon-white.png" />
+            <span>Trailer</span>
+            </Trailer>
+            <AddList>
+              <span />
+              <span />
+            </AddList>
+            <GroupWatch>
+              <div>
+                <img src="/images/group-icon.png" />
+              </div>
+            </GroupWatch>
+          </Controls>
+        </ContentMeta>
     </Conatiner>
   )
 }
@@ -42,7 +63,7 @@ img{
 }
 `
 
-const ImageTitle = styled.div`
+const ImgTitle = styled.div`
 align-items:flex-end ;
 display:flex ;
 -webkit-box-pack:start;
@@ -58,6 +79,122 @@ img{
   min-width:200px ;
   width:35vw ;
 }
+`
+
+const ContentMeta = styled.div`
+max-width:87px ;
+`
+
+const Controls = styled.div`
+align-items:center ;
+display:flex ;
+flex-flow:row nowrap ;
+margin:24px 0px ;
+min-height:56px;
+`
+
+
+const Player = styled.button`
+font-size:15px;
+margin:0px 22px 0px 0px ;
+padding:0px 24px ;
+height:56px ;
+border-radius:4px ;
+
+cursor: pointer;
+display:flex ;
+
+align-items:center ;
+justify-content:center ;
+letter-spacing:1.8px ;
+text-align:center ;
+text-transform:uppercase ;
+background: rgb(249,249,249);
+border:none ;
+color: rgb(0,0,0);
+
+img{
+  width:32px ;
+}
+
+&:hover{
+  background:rgb (198,198,198) ;
+}
+@media(max-width:768px){
+  height:45px ;
+  padding:0px 22px ;
+  font-size:12px ;
+  margin:0px 10px 0px 0px ;
+
+  img{
+    width:25px ;
+  }
+}
 
 `
+const Trailer =styled(Player)`
+background:rgb(0,0,0,0);
+border:1px solid rgb(249,249,249);
+color:rgb(249,249,249) ;
+
+`
+
+const AddList = styled.div`
+margin-right:16px;
+height:44px;
+width:44px;
+display:flex;
+justify-content:center;
+align-items:center;
+background-color:rgb(0,0,0,0);
+border-radius:50%;
+border:2px solid white;
+cursor:pointer;
+
+
+span{
+  background-color:rgb(249,249,249);
+  display:inline-block;
+
+ 
+  &:first-child{
+  height:2px;
+  transform:translate(1px,0px) rotate(0deg);
+  width:16px;
+}
+
+&:nth-child(2){
+  height:16px;
+  transform:translateX(-8px) rotate(0deg);
+  width:2px;
+}
+}
+`
+
+const GroupWatch =styled.div`
+height:44px;
+width:44px;
+border-radius:50%;
+justify-content:center;
+align-items:center;
+cursor:pointer;
+background:white;
+
+div{
+  height:44px;
+width:44px;
+border-radius:50% ;
+background:rgb(0,0,0);
+}
+
+img{
+  width:100%;
+}
+
+`
+
+
+
+
+
 export default Detail
