a45a536a3546ef852f342b2aa7e8dd07d023b775
talhaunas1 <mtalha4600@gmail.com>
2022-07-24 21:14:49 +0500

    styling details component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from './components/Login'
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home'
+import Detail from './components/Detail';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
         <Route path="/" element={<Login/>}/>
         <Route path="/home" element={<Home/>}/>
+        <Route path="/detail/:id" element={<Detail/>}/>
         </Routes>
       </Router>
     </div>
--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,9 +1,63 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Detail() {
   return (
-    <div>Detail</div>
+    <Conatiner>
+        <Background>
+          <img src="" alt="" />
+        </Background>
+        <ImgTitle>
+          <img src="" alt="" />
+        </ImgTitle>
+    </Conatiner>
   )
 }
 
+const Conatiner = styled.div`
+position:relative ;
+min-height:calc(110vh-250px) ;
+overflow:hidden ;
+display:block ;
+top:72px ;
+padding:0 calc(3.5vw + 5px ) ;
+`
+
+const Background = styled.div`
+left:0px;
+opacity:0.8 ;
+position:fixed ;
+right: 0px;
+top:0px ;
+z-index:-1;
+
+img{
+    width:100vw ;
+    height:100vh ;
+
+    @media (max-width:768px)
+    {
+      width:initial ;
+    }
+}
+`
+
+const ImageTitle = styled.div`
+align-items:flex-end ;
+display:flex ;
+-webkit-box-pack:start;
+justify-content:flex-start ;
+margin:0px auto ;
+height:30vw ;
+min-height:170px ;
+padding-bottom:24px ;
+width:100% ;
+
+img{
+  max-width:600px ;
+  min-width:200px ;
+  width:35vw ;
+}
+
+`
 export default Detail
